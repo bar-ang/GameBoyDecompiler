83c784c141e84e4d36a2996573ec8da951c72064
Bar Angel <bar.ang16@gmail.com>
2026-01-27 16:41:42 +0200

    open file only when emulator closes

--- analysis.py
+++ analysis.py
@@ -117,42 +117,44 @@ def get_current_instruction(game):
 
 def main():
     game = PyBoy(GB_FILE)
-    with open(SYM_FILE, "w") as f:
-        while game.tick():
-            inst, disasm = get_current_instruction(game)
+
+    while game.tick():
+        inst, disasm = get_current_instruction(game)
+
+        if inst[0] in LD_OPCODES or inst[0] in ST_OPCODES:
             print(f"{game.register_file.PC:04X}:\t" +
                 f"{" ".join(f"{i:02X}" for i in inst)}\t\t{disasm}")
+            op = disassembler.decode(bytes(inst), 0)
+            if inst[0] == LD_A_IO_A8:
+                key = op.operands[1][1] + 0xff00
+            elif inst[0] == ST_IO_A8_A:
+                key = op.operands[0][1] + 0xff00
+            elif inst[0] in {LD_A_IO_C, ST_IO_C_A}:
+                key = game.register_file.C + 0xff00
+            elif inst[0] in {LD_A_DE, ST_DE_A}:
+                d = game.register_file.D
+                e = game.register_file.E
+                key = (d << 8) & e
+            elif inst[0] in {LD_A_BC, ST_BC_A}:
+                b = game.register_file.B
+                c = game.register_file.C
+                key = (b << 8) & c
+            elif inst[0] == LD_A_A16:
+                key = op.operands[1][1]
+            elif inst[0] == ST_A16_A:
+                key = op.operands[0][1]
+            elif inst[0] in HL_OP_GROUP:
+                key = game.register_file.HL
+            else:
+                raise Exception(f"Unknown opcode {inst[0]:02X}.")
 
-            if inst[0] in LD_OPCODES or inst[0] in ST_OPCODES:
-                op = disassembler.decode(bytes(inst), 0)
-                if inst[0] == LD_A_IO_A8:
-                    key = op.operands[1][1] + 0xff00
-                elif inst[0] == ST_IO_A8_A:
-                    key = op.operands[0][1] + 0xff00
-                elif inst[0] in {LD_A_IO_C, ST_IO_C_A}:
-                    key = game.register_file.C + 0xff00
-                elif inst[0] in {LD_A_DE, ST_DE_A}:
-                    d = game.register_file.D
-                    e = game.register_file.E
-                    key = (d << 8) & e
-                elif inst[0] in {LD_A_BC, ST_BC_A}:
-                    b = game.register_file.B
-                    c = game.register_file.C
-                    key = (b << 8) & c
-                elif inst[0] == LD_A_A16:
-                    key = op.operands[1][1]
-                elif inst[0] == ST_A16_A:
-                    key = op.operands[0][1]
-                elif inst[0] in HL_OP_GROUP:
-                    key = game.register_file.HL
-                else:
-                    raise Exception(f"Unknown opcode {inst[0]:02X}.")
+            if key not in symbols:
+                symbols[key] = gen_var("Temp")
 
-                if key not in symbols:
-                    symbols[key] = gen_var()
-                    f.write(f"{key:04X} {symbols[key]}\n")
-
-        game.stop()
+    game.stop()
+    with open(SYM_FILE, "w") as f:
+        for k, v in symbols.items():
+            f.write(f"{k:04X} {v}\n")
 
 if __name__ == "__main__":
     main()
