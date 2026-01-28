5a905156485f86daea1f7c67aa204263f40127cd
Bar Angel <bar.ang16@gmail.com>
2026-01-28 20:53:31 +0200

    more readable output

--- src/analyzer.py
+++ src/analyzer.py
@@ -116,4 +116,4 @@ class Expr:
 if __name__ == "__main__":
     ast = AST()
     res = ast.process_all([0xA5, 0x86, 0xA0, 0x88])
-    print(ast.rA)
+    print("\n".join([f"{k} : {v}" for k, v in ast._data.items()]))
