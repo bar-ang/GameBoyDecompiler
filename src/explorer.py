5aad5551e46139977663644af7d755b3b112cc3d
Bar Angel <bar.ang16@gmail.com>
2026-02-01 18:43:54 +0200

    add a function explorer!

---
+++ src/explorer.py
@@ -0,0 +1,37 @@
+CHUNK_SIZE = 256
+
+def search_inf_loop(buff):
+    for i, opcode in enumerate(buff[:-1]):
+        if opcode == 0x18 and buff[i+1] >= 128:
+            # absolute JR to previous location
+            return i
+    return None
+
+def explore(file, pc_start=0x100, main_func="main"):
+    funcmap = {}
+
+    f.seek(pc_start)
+
+    jr_pos = None
+    while not jr_pos:
+        buff = f.read(CHUNK_SIZE)
+        assert buff, "EOF and function not identified"
+        jr_pos = search_inf_loop(buff)
+
+    print(buff[jr_pos:jr_pos+2])
+    end = pc_start + jr_pos
+    funcmap[main_func] = (pc_start, end)
+
+    return funcmap
+#{
+#    main: (begin, end)
+#    UNREACHABLE0: (0x192, end)
+#    UNREACHABLE1: (0x4020, end)
+#    Gerbil (Func_0): (begin, end)
+#}
+
+
+if __name__ == "__main__":
+    with open("example.gb", "rb") as f:
+        funcmap = explore(f)
+    print(funcmap)
