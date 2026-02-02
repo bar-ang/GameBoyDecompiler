1b41be653dc89adb1a0dfa607cf9f5e5b8d17dcd
Bar Angel <bar.ang16@gmail.com>
2026-02-02 17:21:55 +0200

    I have big plans for the 'Expr' class, so I moved it to a dedicated file

--- src/gb_ast.py
+++ src/gb_ast.py
@@ -1,4 +1,4 @@
-import re
+from expr import Expr
 from opcodes import *
 from lr35902dis import lr35902 as disassembler
 
@@ -293,41 +293,6 @@ class AST:
     def process_all(self, code):
         while len(code) > 0:
             code = self.step(code)
-
-
-class Expr:
-    def __init__(self, op, a, b=None, *, postpositive=False):
-        self.op = op
-        self.a = a
-        self.b = b
-        self._postpositive = postpositive
-
-    def __repr__(self):
-        return self.__str__()
-
-    def __str__(self):
-        regex = r'[A-Za-z0-9&\.]+'
-        if type(self.a) == str:
-            a_str = self.a
-        else:
-            a_str = self.a.__str__()
-
-        if not re.fullmatch(regex, a_str):
-            a_str = f"({a_str})"
-
-        if self.b is None:
-            return f"{self.op}{a_str}"if not self._postpositive else f"{a_str}{self.op}"
-
-        if type(self.b) == str:
-            b_str = self.b
-        else:
-            b_str = self.b.__str__()
-
-        if not re.fullmatch(regex, b_str):
-            b_str = f"({b_str})"
-
-        return f"{a_str}{self.op}{b_str}" if not self._postpositive else f"{a_str}{b_str}{self.op}"
-
 
 
 if __name__ == "__main__":
