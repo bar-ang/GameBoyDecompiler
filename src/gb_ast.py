535dee0e09d55cbfb111af95282084fdaeb6093c
Bar Angel <bar.ang16@gmail.com>
2026-02-11 15:10:30 +0200

    ast: very simple Jump handler

--- src/gb_ast.py
+++ src/gb_ast.py
@@ -1,5 +1,7 @@
 import textwrap as tw
+import syntax
 from expr import Expr
+
 
 INDENT = " " * 4
 
@@ -49,6 +51,28 @@ class ASTNodeText(ASTNode):
     def __str__(self):
         return self.text
 
+class ASTNodeJumpHandler(ASTNode):
+    def __init__(self, inst, **regs):
+        assert type(inst) in {syntax.InstRelJumpConditional, syntax.InstRelJump}
+        self.inst = inst
+        self.regs = regs
+
+    def __str__(self):
+        if type(self.inst) == syntax.InstRelJump:
+            return "true"
+        else:
+            if self.inst.cond == "C":
+                return "A >= 0"
+            elif self.inst.cond == "NC":
+                return "A < 0"
+            elif self.inst.cond == "Z":
+                return "A != 0"
+            elif self.inst.cond == "NZ":
+                return "A == 0"
+            else:
+                raise Exeption("unknown condition")
+            return str(self.inst)
+
 def make_scope_for_func(content):
     scope = []
     for inst, _ in content:
@@ -58,7 +82,7 @@ def make_scope_for_func(content):
             assert "type" in inst
             assert inst["type"].upper() in ("IF", "LOOP")
             inner_scope = make_scope_for_func(inst["content"])
-            cond = ASTNodeText(str(inst["inst"]))
+            cond = ASTNodeJumpHandler(inst["inst"])
             if inst["type"].upper() == "IF":
                 scope.append(ASTNodeIfStmt(cond, inner_scope))
             else:
