35c27d2633ae5f482e90b3091d1671836505f1aa
Bar Angel <bar.ang16@gmail.com>
2026-02-11 11:11:04 +0200

    ast: now the AST has children

--- src/gb_ast.py
+++ src/gb_ast.py
@@ -2,22 +2,44 @@ from expr import Expr
 from lr35902dis import lr35902 as disassembler
 
 class ASTNode:
-    def __init__(self, op, scope, **args):
-        self.op = op
+    def __init__(self, scope):
         self.scope = scope
-        self.args = args
+
+    def content(self, depth=1):
+        return "\n".join(["\t"*depth + str(c) for c in self.scope])
 
     def __str__(self):
-        content = "\n".join([str(c) for c in self.scope])
-        if self.op == "initial":
-            return content
-        elif self.op == "func":
-            return f"{self.args["name"]} {{\n\t{content}\n}}"
-        assert False, "unhandled"
+        return "???"
 
+class ASTNodeInitial(ASTNode):
+    def __str__(self):
+        return self.content(0)
+
+class ASTNodeFunc(ASTNode):
+    def __init__(self, name, scope):
+        super().__init__(scope)
+        self.name = name
+
+    def __str__(self):
+        return f"{self.name} {{\n{self.content()}\n}}"
+
+class ASTNodeLoop(ASTNode):
+    def __init__(self, scope):
+        super().__init__(scope)
+
+    def __str__(self):
+        return f"while(...) {{\n{self.content()}\n}}"
+
+
+class ASTNodeCondition(ASTNode):
+    def __init__(self, scope):
+        super().__init__(scope)
+
+    def __str__(self):
+        return f"if(...) {{\n{self.content()}\n}}"
 
 def build_ast(explored_tokens):
     scope = []
     for func, content in explored_tokens.items():
-        scope.append(ASTNode("func", name=func, scope=[]))
-    return ASTNode("initial", scope=scope)
+        scope.append(ASTNodeFunc(name=func, scope=[]))
+    return ASTNodeInitial(scope=scope)
