a8aec1774c41575a2aefb89c08c4bac60b2c4222
Bar Angel <bar.ang16@gmail.com>
2026-02-14 16:47:03 +0200

    example: ast_tiny.asm

--- examples/ast_tiny.asm
+++ examples/ast_tiny.asm
@@ -6,6 +6,7 @@ SECTION "The begining", ROM0[$120]
 
 start:
 	ld a, $77
+	xor 8
 	ld [$CAFE], a
 
 	ld a, [$BAB0]
@@ -30,6 +31,22 @@ start:
 	ld a, e
 	ld [$CAFE], a
 
+	ld a, [$f000]
+	ld b, a
+	ld a, [$f001]
+	and b
+	ld [$f002], a
+	sla a
+	ld [$f003], a
+	cpl
+	ld [$f004], a
+	sub a, b
+	ld [$f005], a
+
+	ld hl, $f000
+	db 0x34 ; inc (hl)
+	db 0x35 ; dec (hl)
+	
 inf:
 	nop
 	jr inf
