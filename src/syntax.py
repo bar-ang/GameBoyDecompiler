3fb40992925cdb5db10ec34915f63bd7ac66012e
Bar Angel <bar.ang16@gmail.com>
2026-02-17 22:10:47 +0200

    syntax.py: this became unworkable - refactoring the entire syntax

--- src/syntax.py
+++ src/syntax.py
@@ -2,18 +2,46 @@ from abc import ABC, abstractmethod
 from expr import Expr
 from enum import Enum
 
-ALU = {
-    "ADD": "+",
-    "ADC": "+`",
-    "SUB": "-",
-    "SBC": "-`",
-    "AND": "&",
-    "OR" : "|",
-    "XOR": "^",
-    "CPL": "!"
-}
+class Operator(Enum):
+    LD = "LD"
+    JP = "JP"
+    JR = "JR"
+    CALL = "CALL"
+    RET = "RET"
+    RETI = "RETI"
+    RST = "RST"
+    RLCA = "RLCA"
+    RLA  = "RLA"
+    RRCA = "RRCA"
+    RRA  = "RRA"
+    DAA = "DAA"
+    SCF = "SCF"
+    CCF = "CCF"
+    INC = "INC"
+    DEC = "DEC"
+    POP = "POP"
+    PUSH = "PUSH"
+    ADD = "ADD"
+    ADC = "ADC"
+    SUB = "SUB"
+    SBC = "SBC"
+    AND = "AND"
+    OR = "OR"
+    XOR = "XOR"
+    CPL = "CPL"
+    CP = "CP"
+    NOP = "NOP"
+    STOP = "STOP 0"
+    HALT = "HALT"
+    CB = "CB"
+    DI = "DI"
+    EI = "EI"
+    BETA = "β"
 
-class Regs(Enum):
+    def __str__(self):
+        return f"{self.value}"
+
+class Reg(Enum):
     A = "A"
     B = "B"
     C = "C"
@@ -27,381 +55,221 @@ class Regs(Enum):
     PC = "PC"
     Stack = "Stack"
 
+    def __str__(self):
+        return f"{self.value}"
+
+class Direct(Enum):
+    BC = (Reg.B, Reg.C, 0)
+    DE = (Reg.D, Reg.E, 0)
+    AF = (Reg.A, Reg.F, 0)
+    HL = (Reg.H, Reg.L, 0)
+    HLPlus = (Reg.H, Reg.L, 1)
+    HLMinus = (Reg.H, Reg.L, -1)
+
+    def __str__(self):
+        suff = ""
+        if self.value[2] > 0:
+            suff = "+"
+        elif self.value[2] < 0:
+            suff = "-"
+        return f"{self.value[0]}{self.value[1]}{suff}"
+
+class Cond(Enum):
+    C = "C"
+    Z = "Z"
+    NC = "NC"
+    NZ = "NZ"
+
+    def __str__(self):
+        return f"{self.value}"
+
+class Deref:
+    def __init__(self, operand : Reg | Direct | int):
+        self._operand = operand
+
+    @property
+    def operand(self):
+        return self._operand
+
+    def __str__(self):
+        return f"[{self.operand}]"
+
+OpdType = Reg | Deref | Direct | Cond | int
+
+
 def create_initial_regmap():
     return {
-#        Regs.A : 0x11,
-#        Regs.B : 0,
-#        Regs.C : 0,
-#        Regs.D : 0xff,
-#        Regs.E : 0x56,
-#        Regs.F : 0x80,
-#        Regs.H : 0,
-#        Regs.L : 0xd,
-#        Regs.SP : 0xfffe,
-#        Regs.PC : 0x100,
-        "A" : 0x11,
-        "B" : 0,
-        "C" : 0,
-        "D" : 0xff,
-        "E" : 0x56,
-        "F" : 0x80,
-        "H" : 0,
-        "L" : 0xd,
-        "PC" : 0x100,
-        "Stack": []
+        Reg.A : 0x11,
+        Reg.B : 0,
+        Reg.C : 0,
+        Reg.D : 0xff,
+        Reg.E : 0x56,
+        Reg.F : 0x80,
+        Reg.H : 0,
+        Reg.L : 0xd,
+        Reg.SP : 0xfffe,
+        Reg.PC : 0x100,
+        Reg.Stack: []
     }
 
 class Instruction(ABC):
 
-    def __init__(self, op:str, regl:str=None, regr:str=None, *, imm:int=0, addr:int=0, cond:str=""):
+    def __init__(self,
+                 op: Operator | None=None,
+                 left: OpdType | None=None,
+                 right: OpdType | None=None):
         self._op = op
-        self.regl = regl
-        self.regr = regr
-        self.imm = imm
-        self.addr = addr
-        self.cond = cond
+        self.left = left
+        self.right = right
 
     @property
-    def reg(self):
-        assert (self.regl or self.regr) and not (self.regl and self.regr)
-        return self.regl if self.regl else self.regr
+    def operand(self):
+        assert (self.left or self.right) and not (self.left and self.right)
+        return self.left or self.right
 
     @property
     def op(self):
         return self._op
 
+    def __repr__(self):
+        return self.__str__()
+
     def __str__(self):
-        return f"{self.op} {self.cond} {self.regl}, {self.regr}, {self.imm:02x}, ({self.addr:04x})"
+        res = f"{self.op}"
+        if self.left:
+            res += f" {self.left}"
+        if self.right:
+            res += f", {self.right}"
+
+        return res
 
     def dry_run(self, regmap):
         pass
 
 
-class InstFamilyOpOnly(Instruction):
-    def __init__(self, op):
-        return super().__init__(op)
+class InstControl(Instruction):
+    def __init__(self, op: Operator):
+        super().__init__(op)
 
-    def __str__(self):
-        return f"{self.op}"
 
+class InstFlow(Instruction):
+    def __init__(self, op: Operator, cond: Cond | None=None, addr: int | None=None):
+        super().__init__(op, cond, addr)
 
-class InstFamilySingleReg(Instruction):
-    def __init__(self, op, reg="A"):
-        return super().__init__(op, reg)
 
-    def __str__(self):
-        return f"{self.op} {self.regl}"
+class InstConditionalJr(InstFlow):
+    def __init__(self, cond: Cond, addr: int):
+        super().__init__(Operator.JR, cond, addr)
 
 
-class InstFamilyTwoRegs(Instruction):
-    def __init__(self, op, regl="A", regr="A"):
-        return super().__init__(op, regl, regr)
+class InstConditionalJp(InstFlow):
+    def __init__(self, cond: Cond, addr: int):
+        super().__init__(Operator.JP, cond, addr)
 
-    def __str__(self):
-        return f"{self.op} {self.regl}, {self.regr}"
 
+class InstJr(InstFlow):
+    def __init__(self, addr: int):
+        super().__init__(Operator.JR, addr=addr)
 
-class InstFamilyRegWithImmediate(Instruction):
-    def __init__(self, op, reg="A", imm=0):
-        return super().__init__(op, reg, imm=imm)
 
-    def __str__(self):
-        return f"{self.op} {self.regl}, ${self.imm:04x}"
+class InstJp(InstFlow):
+    def __init__(self, addr: int):
+        super().__init__(Operator.JP, addr=addr)
 
 
-class InstFamilyAddr(Instruction):
-    def __init__(self, op, addr):
-        return super().__init__(op, addr=addr)
+class InstCall(InstFlow):
+    def __init__(self, addr: int):
+        super().__init__(Operator.CALL, addr=addr)
 
-    def __str__(self):
-        return f"{self.op} ${self.addr:04x}"
 
+class InstRet(InstFlow):
+    def __init__(self):
+        super().__init__(Operator.RET)
 
-class InstFamilyDirect(Instruction):
-    def __init__(self, op, reg="HL"):
-        return super().__init__(op, reg)
+class InstReti(InstFlow):
+    def __init__(self):
+        super().__init__(Operator.RET)
 
-    def __str__(self):
-        return f"{self.op} ({self.regl})"
 
+class InstConditionalRet(InstFlow):
+    def __init__(self, cond: Cond):
+        super().__init__(Operator.RET, cond)
 
-class InstFamilyStoreReg(Instruction):
-    def __init__(self, op, regl="HL", regr="A"):
-        return super().__init__(op, regl, regr)
 
-    def __str__(self):
-        return f"{self.op} ({self.regl}), {self.regr}"
+class InstConditionalCall(InstFlow):
+    def __init__(self, cond: Cond, addr: int):
+        super().__init__(Operator.CALL, addr=addr)
 
 
-class InstFamilyLoadReg(Instruction):
-    def __init__(self, op, regl="A", regr="HL"):
-        return super().__init__(op, regl, regr)
+class InstLd8bit(Instruction):
+    def __init__(self, left: OpdType, right: OpdType):
+        super().__init__(Operator.LD, left, right)
 
-    def __str__(self):
-        return f"{self.op} {self.regl}, ({self.regr})"
 
+class InstLdhLoad(InstLd8bit):
+    def __init__(self, addr: int):
+        super().__init__(Reg.A, addr + 0xff00)
 
-class InstFamilyStoreAddr(Instruction):
-    def __init__(self, op, addr, reg):
-        return super().__init__(op, regr=reg, addr=addr)
 
-    def __str__(self):
-        return f"{self.op} (${self.addr:04x}), {self.regr}"
+class InstLdhStore(InstLd8bit):
+    def __init__(self, addr: int):
+        super().__init__(addr + 0xff00, Reg.A)
 
 
-class InstFamilyLoadAddr(Instruction):
-    def __init__(self, op, reg, addr):
-        return super().__init__(op, regl=reg, addr=addr)
+class InstLdhCLoad(InstLd8bit):
+    def __init__(self):
+        super().__init__(Reg.A, Deref(Reg.C))
 
-    def __str__(self):
-        return f"{self.op} {self.regr}, (${self.addr:04x})"
 
+class InstLdhCStore(InstLd8bit):
+    def __init__(self):
+        super().__init__(Deref(Reg.C), Reg.A)
 
-class InstFamilyStoreImm(Instruction):
-    def __init__(self, op, reg, imm):
-        return super().__init__(op, reg, imm=imm)
+class InstLd16bit(InstLd8bit):
+    def __init__(self, left: Direct, right: Direct):
+        super().__init__(left, right)
 
-    def __str__(self):
-        return f"{self.op} {self.regl}, ${self.imm:04x}"
 
+class InstPop(Instruction):
+    def __init__(self, direct: Direct):
+        super().__init__(Operator.POP, left=direct)
 
-class InstFamilyCondition(Instruction):
-    def __init__(self, op, cond, addr=0):
-        return super().__init__(op, cond=cond, addr=addr)
 
-    def __str__(self):
-        return f"{self.op} {self.cond}, ${self.addr:04x}"
+class InstPush(Instruction):
+    def __init__(self, direct: Direct):
+        super().__init__(Operator.PUSH, left=direct)
 
 
+class InstALU8bit(Instruction):
+    def __init__(self, op: Operator, operand: OpdType=Reg.A):
+        super().__init__(op, left=Reg.A, right=operand)
 
-class InstALUregSP(InstFamilyRegWithImmediate):
-    # NOTE: a rare command (E8h), barely used
-    pass
 
+class InstALU16bit(Instruction):
+    def __init__(self, op: Operator, operand: Direct):
+        super().__init__(op, left=Direct.HL, right=operand)
 
-class InstALU(InstFamilyTwoRegs):
-    def dry_run(self, regmap):
-        assert self.op in ALU, f"unknown ALU {self.op}"
-        regmap[self.regl] = Expr(ALU[self.op], regmap[self.regl], regmap[self.regr])
-        return None
 
-class InstALU16bit(InstFamilyTwoRegs):
-    pass
+class InstRotShift(Instruction):
+    def __init__(self, op: Operator, operand: OpdType=Reg.A):
+        super().__init__(op, left=operand)
 
 
-class InstALUDirect(InstFamilyLoadReg):
-    def dry_run(self, regmap):
-        assert self.op in ALU, f"unknown ALU {self.op}"
-        assert self.regl in regmap, self.regl
-        val = Expr("*", regmap[self.regr])
-        regmap[self.regl] = Expr(ALU[self.op], regmap[self.regl], val)
-        return None
+class InstRst(Instruction):
+    def __init__(self, imm:int):
+        super().__init__(Operator.RST, left=imm)
 
 
-class InstALUImmediate(InstFamilyRegWithImmediate):
-    def dry_run(self, regmap):
-        assert self.op in ALU, f"unknown ALU {self.op}"
-        assert self.regl in regmap, self.regl
-        regmap[self.regl] = Expr(ALU[self.op], regmap[self.regl], f"${self.imm:02x}")
-        return None
+class InstInc(Instruction):
+    def __init__(self, reg: OpdType):
+        super().__init__(Operator.INC, reg)
 
 
-class InstIncDec(InstFamilySingleReg):
-    def dry_run(self, regmap):
-        assert self.regl in regmap
-        val = regmap[self.regl]
-        sign = "+" if self.op.upper() == "INC" else "-"
-        val = Expr(sign, val, "1")
-        regmap[self.regl] = val
-        return None
+class InstDec(Instruction):
+    def __init__(self, reg: OpdType):
+        super().__init__(Operator.DEC, reg)
 
 
-class InstIncDecDirect(InstFamilyDirect):
-    def dry_run(self, regmap):
-        assert self.reg in regmap or \
-            (len(self.reg) == 2 and self.reg[0] in regmap and \
-            self.reg[1] in regmap)
-        val = Expr("*", regmap[self.reg[0]])
-        sign = "+" if self.op.upper() == "INC" else "-"
-        val = Expr(sign, val, "1")
-        return Expr(":=", regmap[self.reg[0]], val)
-
-
-class InstIncDec16bit(InstFamilySingleReg):
-    pass
-
-
-class InstCBPrefixDirect(InstFamilyDirect):
-    def dry_run(self, regmap):
-        assert self.reg in regmap
-        val = Expr("*", regmap[self.reg])
-        val = Expr(self.op, val)
-        return Expr(":=", regmap[self.reg], val)
-
-
-class InstCBPrefix(InstFamilySingleReg):
-    def dry_run(self, regmap):
-        assert self.reg in regmap
-        regmap[self.reg] = Expr(self.op, regmap[self.reg])
-        return None
-
-
-class InstRelJumpConditional(InstFamilyCondition):
-    def __init__(self, op, cond, addr):
-        if addr >= 128:
-            addr = addr - 256
-        return super().__init__(op, cond=cond, addr=addr)
-
-
-class InstAbsJumpConditional(InstFamilyCondition):
-    pass
-
-
-class InstRelJump(InstFamilyAddr):
-    def __init__(self, op, addr):
-        if addr >= 128:
-            addr = addr - 256
-        return super().__init__(op, addr=addr)
-
-    def dry_run(self, regmap):
-        return None
-
-
-class InstAbsJump(InstFamilyAddr):
-    def dry_run(self, regmap):
-        return None
-
-
-class InstPush(InstFamilyTwoRegs):
-    def __init__(self, op, highreg, lowreg):
-        super().__init__(op, regl=highreg, regr=lowreg)
-
-    def dry_run(self, regmap):
-        regmap["Stack"].append((self.regl, self.regr))
-
-class InstPop(InstFamilyTwoRegs):
-    def __init__(self, op, highreg, lowreg):
-        super().__init__(op, regl=highreg, regr=lowreg)
-
-    def dry_run(self, regmap):
-        l, r = regmap["Stack"][-1]
-        regmap["Stack"] = regmap["Stack"][:-1]
-        regmap[self.regl] = regmap[l]
-        regmap[self.regr] = regmap[r]
-
-
-class InstLoadImmediate16bit(InstFamilyRegWithImmediate):
-    def dry_run(self, regmap):
-        assert self.reg in regmap or \
-            (len(self.reg) == 2 and self.reg[0] in regmap and \
-            self.reg[1] in regmap)
-        regmap[self.reg[0]] = self.imm & 0xff
-        regmap[self.reg[1]] = self.imm >> 8
-
-        return None
-
-class InstLoadImmediate(InstFamilyRegWithImmediate):
-    def dry_run(self, regmap):
-        assert self.regl in regmap, self.regl
-        regmap[self.regl] = f"${self.imm:02x}"
-        return None
-
-class InstLoadDirect(InstFamilyLoadReg):
-    pass
-
-
-class InstLoadRegToReg(InstFamilyTwoRegs):
-    def dry_run(self, regmap):
-        regmap[self.regl] = regmap[self.regr]
-        return None
-
-class InstLoadRegToHL(InstFamilyStoreReg):
-    pass
-
-
-class InstLoadHLToReg(InstFamilyLoadReg):
-    pass
-
-
-class InstLoadRegToHLI(InstFamilyStoreReg):
-    pass
-
-
-class InstLoadHLIToReg(InstFamilyLoadReg):
-    pass
-
-
-class InstLoadImmediateDirect(Instruction):
-    def __init__(self, op, imm, reg="HL"):
-        return super().__init__(op, regl=reg, imm=imm)
-
-    def __str__(self):
-        return f"{self.op} ({self.regl}), {self.imm:02x}"
-    
-
-class InstLoad16bit(InstFamilyLoadReg):
-    pass
-
-
-class InstStore16bit(InstFamilyStoreReg):
-    pass
-
-
-class InstLoadAddr(InstFamilyLoadAddr):
-    def dry_run(self, regmap):
-        assert self.regl in regmap, self.regl
-        regmap[self.regl] = f"*${self.addr:02x}"
-        return None
-
-
-class InstStoreAddr(InstFamilyStoreAddr):
-    def dry_run(self, regmap):
-        assert self.regr in regmap
-        val = Expr(":=", f"${self.addr:04x}", regmap[self.regr])
-        return val
-
-
-class InstHighLoad(InstFamilyLoadAddr):
-    pass
-
-
-class InstHighStore(InstFamilyStoreAddr):
-    pass
-
-
-class InstHighCStore(InstFamilyStoreReg):
-    pass
-
-
-class InstHighCLoad(InstFamilyLoadReg):
-    pass
-
-
-class InstReset(InstFamilyRegWithImmediate):
-    pass
-
-
-class InstControl(InstFamilyOpOnly):
-    def dry_run(self, regmap):
-        if self.op.upper() == "NOP":
-            return None
-
-        raise Exception(f"unimplemented dry run for op '{self.op}'")
-
-class InstCall(InstFamilyAddr):
-    pass
-
-
-class InstRet(InstFamilyOpOnly):
-    pass
-
-
-class InstConitionalRet(InstFamilyCondition):
-    def __str__(self):
-        return f"{self.op} {self.cond}"
-
-
-class InstConitionalCall(InstFamilyCondition):
-    pass
-
-
+class InstAddRegSP(InstALU8bit):
+    def __init__(self, r8: int):
+        super().__init__(Operator.ADD, operand=r8)
