eb356be39f8dfdf2306495e0e117b24567796f08
Bar Angel <bar.ang16@gmail.com>
2026-02-24 16:44:46 +0200

    new expr class

--- src/expr.py
+++ src/expr.py
@@ -1,35 +1,71 @@
+from __future__ import annotations
 import re
 
+TypeBaseExpr = int | str
+
+
 class Expr:
-    def __init__(self, op, a, b=None, *, postpositive=False):
+    def __init__(self, op: TypeBaseExpr,
+                a: Expr | None=None,
+                b: Expr | None=None,
+                *, postpositive: bool=False,
+                size_bytes: int=4):
         self.op = op
         self.a = a
         self.b = b
         self._postpositive = postpositive
+        self.size_bytes = size_bytes
+
+    def op_as_str(self):
+        if type(self.op) == str:
+            return self.op
+
+        assert type(self.op) == int
+        return f"${self.op:0{self.size_bytes}X}"
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        regex = r'[A-Za-z0-9&\.]+'
-        if type(self.a) == str:
-            a_str = self.a
+        if self.a is not None:
+            str_a = str(self.a)
         else:
-            a_str = self.a.__str__()
+            str_a = ""
 
-        if not re.fullmatch(regex, a_str):
-            a_str = f"({a_str})"
-
-        if self.b is None:
-            return f"{self.op}{a_str}"if not self._postpositive else f"{a_str}{self.op}"
-
-        if type(self.b) == str:
-            b_str = self.b
+        if self.b is not None:
+            str_b = str(self.b)
         else:
-            b_str = self.b.__str__()
+            str_b = ""
 
-        if not re.fullmatch(regex, b_str):
-            b_str = f"({b_str})"
+        regex = r'[A-Za-z0-9&\. ]+'
 
-        return f"{a_str}{self.op}{b_str}" if not self._postpositive else f"{a_str}{b_str}{self.op}"
+        if str_a and not re.fullmatch(regex, str_a):
+            str_a = f"({str_a})"
+
+        if str_b and not re.fullmatch(regex, str_b):
+            str_b = f"({str_b})"
+
+        return f"{str_a}{self.op_as_str()}{str_b}"
+
+
+def main():
+    a = Expr("a")
+    b = Expr("b")
+    c = Expr(0xbec)
+    t = Expr("+", a, b)
+    t2 = Expr("*", c, t)
+
+    print(a)
+    print(b)
+    print(c)
+
+    print(t)
+    print(t2)
+
+    print(Expr("++", 5))
+
+    return 0
+
+if __name__ == "__main__":
+    main()
 
