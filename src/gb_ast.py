fbdb8fe38ceb0685683273db371764221a9393b4
Bar Angel <bar.ang16@gmail.com>
2026-03-07 21:11:13 +0200

    AST: improving make_scope_for_func

--- src/gb_ast.py
+++ src/gb_ast.py
@@ -69,29 +69,43 @@ class ASTNodeJumpHandler(ASTNode):
         super().__init__(scope=[])
 
 
-def make_scope_for_func(content_begin: Token, regmap: syntax.TypeRegmap, stack: syntax.TypeStack, token_end: Token | None=None) -> list:
+def make_scope_for_func(content_begin: Token,
+                        regmap: syntax.TypeRegmap,
+                        stack: syntax.TypeStack,
+                        token_end: Token | None=None) -> list:
     scope: list = []
     tok = content_begin
     while tok is not None and tok is not token_end:
-        if tok.next_cond_token:
-            after = tok.next_cond_token
-            cond = ASTNodeExpression(
-                Expr(syntax.CONDITIONS[tok.inst.cond],
-                     regmap[syntax.Reg.A], Expr(0)
+        if tok.jump_addr:
+            if tok.if_cond_unmet:
+                after = tok.next_feature
+                cond = ASTNodeExpression(
+                    Expr(syntax.CONDITIONS[tok.inst.cond],
+                         regmap[syntax.Reg.A], Expr(0)
+                    )
                 )
-            )
-            if_scope = make_scope_for_func(
-                tok.next_token, regmap, stack, token_end=after
-            )
-            scope.append(ASTNodeIfStmt(cond, scope=if_scope))
-            tok = after
+
+                if_scope = make_scope_for_func(
+                    tok.jump_addr, regmap, stack, token_end=after
+                )
+
+                else_scope = make_scope_for_func(
+                    tok.if_cond_unmet, regmap, stack, token_end=after
+                )
+
+                # NOTE: we intentionally reverse the condition statement
+                # and flip the if and else
+                scope.append(ASTNodeIfStmt(cond, else_scope, if_scope))
+                tok = after
+            else:
+                tok = tok.jump_addr
         else:
             expr = tok.inst.dry_run(regmap)
             if isinstance(tok.inst, syntax.InstStack):
                 tok.inst.update_stack(regmap, stack)
             if expr:
                 scope.append(ASTNodeExpression(expr))
-            tok = tok.next_token
+            tok = tok.next_feature
 
     return scope
 
