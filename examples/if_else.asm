2b23117ddd739cb73acc61e46a5f40dd0e69f3c8
Bar Angel <bar.ang16@gmail.com>
2026-03-08 12:42:54 +0200

    add example for IF with ELSE clause

---
+++ examples/if_else.asm
@@ -0,0 +1,25 @@
+SECTION "The thing", ROM0[$100]
+	nop
+	jp start
+	
+SECTION "The begining", ROM0[$120]
+
+start:
+
+	ld a, [$1]
+	jr nz, elseclause
+
+	ld [$2], a
+
+	jr after
+elseclause:
+	
+	ld [$3], a
+after:
+	ld a, [$f]
+	inc a
+	ld [$f], a
+
+inf:
+	nop
+	jr inf
